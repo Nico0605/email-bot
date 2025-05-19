a4fe710294a9888ddcfe43e579fecb6e0461a5b1
Nicolò Fiore <fiorenicolo05@gmail.com>
2025-05-19 19:07:35 +0200

    functional bot

--- src/main/java/org/miesgroup/emailbot/application/EmailBot.java
+++ src/main/java/org/miesgroup/emailbot/application/EmailBot.java
@@ -3,14 +3,19 @@ package org.miesgroup.emailbot.application;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import static io.quarkus.arc.impl.UncaughtExceptions.LOGGER;
+import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @QuarkusMain
 public class EmailBot implements QuarkusApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EmailBot.class);
+
     @Override
     public int run(String... args) {
-        LOGGER.info("✅ Applicazione avviata. In attesa delle attività schedulate...");
+        System.setProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager");
+        LOG.info("✅ Applicazione avviata. In attesa delle attività schedulate...");
         Quarkus.waitForExit();
         return 0;
     }
@@ -19,3 +24,4 @@ public class EmailBot implements QuarkusApplication {
         Quarkus.run(EmailBot.class, args);
     }
 }
+
