4411f0c5b4dcb161e4db9d8a922d528b4d50e1ee
Nicolò Fiore <fiorenicolo05@gmail.com>
2025-05-26 17:15:39 +0200

    .

--- src/main/java/org/miesgroup/emailbot/application/EmailBot.java
+++ src/main/java/org/miesgroup/emailbot/application/EmailBot.java
@@ -3,16 +3,11 @@ package org.miesgroup.emailbot.application;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import jakarta.inject.Inject;
-import org.miesgroup.emailbot.service.email.EmailManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @QuarkusMain
 public class EmailBot implements QuarkusApplication {
-
-    @Inject
-    EmailManager emailManager;
 
     private static final Logger LOG = LoggerFactory.getLogger(EmailBot.class);
 
@@ -20,7 +15,6 @@ public class EmailBot implements QuarkusApplication {
     public int run(String... args) {
         System.setProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager");
         LOG.info("✅ Applicazione avviata. In attesa delle attività schedulate...");
-        emailManager.DailyEmailAlert();
         Quarkus.waitForExit();
         return 0;
     }
